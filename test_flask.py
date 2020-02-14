f29cce38d24b480994af735ed9f7a374d043d301
awaji-mitop GILBERT <successgilli@gmail.com>
2020-02-13 18:28:38 -0800

    added extra test

--- test_flask.py
+++ test_flask.py
@@ -7,6 +7,12 @@ class TestApp(unittest.TestCase):
     def test_land(self):
         apptest = app.test_client(self)
         res = apptest.get('/')
-        self.assertEqual(res.data, b'welcome home')
+        self.assertEqual(res.status, '200 OK')
+    
+    def test_body(self):
+        tester = app.test_client(self)
+        res = tester.get('/')
+        print(res)
+        self.assertEqual(res.data, b'{"data":"welcome","status":200}\n')
 
 if __name__ == '__main__': unittest.main()
